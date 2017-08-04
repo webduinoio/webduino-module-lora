8972b7d116b016325a5e6bc40974df6f8d560420
osk2 <sc60714@gmail.com>
2017-08-04 12:22:02 +0800

    Fix typo

--- blockly/msg/blocks/en.js
+++ blockly/msg/blocks/en.js
@@ -3,4 +3,4 @@ Blockly.Msg.WEBDUINO_LORA_SEND_STRING = "Send string";
 Blockly.Msg.WEBDUINO_LORA_SUCCESS_CALLBACK = "After successed, do";
 Blockly.Msg.WEBDUINO_LORA_FAIL_CALLBACK = "After failed, do";
 Blockly.Msg.WEBDUINO_LORA_RECEIVE_CALLBACK = "After receiving string, do";
-Blockly.Msg.WEBDUINO_LORA_RECEIVED_STRING = "Redeived string";
+Blockly.Msg.WEBDUINO_LORA_RECEIVED_STRING = "Received string";
